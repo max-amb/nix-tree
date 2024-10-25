989883fa25ef6e2bbdebaf48e5b455c8d5d81828
Max Ambaum <max_a@e.email>
2024-10-25 14:24:41 +0100

    Removed crazy error from errors and added one for comment handling

--- nix_tree/errors.py
+++ nix_tree/errors.py
@@ -12,18 +12,6 @@ class NodeNotFound(Exception):
     def __init__(self, node_name: str, message: str = "Node {NODE} does not exist in the tree"):
         super().__init__(message.format(NODE=node_name))
 
-
-class CrazyError(Exception):
-    """Raised when I have no idea what has happened, should never be raised
-
-    Args:
-        message: str - the message to print out with this exception
-    """
-
-    def __init__(self, message: str = "How on gods green earth did you get here"):
-        super().__init__(message)
-
-
 class NoValidHeadersNode(Exception):
     """Raised if no valid headers are found by the composer
 
@@ -39,8 +27,17 @@ class ErrorComposingFileFromTree(Exception):
     Args:
         message: str - the message to print out with this exception
     """
-    def __init__(self, message: str = "an error was encountered composing the file from the tree") -> None:
+    def __init__(self, message: str = "An error was encountered composing the file from the tree") -> None:
         super().__init__(message)
 
 class ConfigurationFileNotFound(Exception):
     """Raised if no configuration file is found to edit/read"""
+
+class ErrorHandlingComments(Exception):
+    """Raised if there was an error with comment management
+
+    Args:
+        message: str - the message to print out with this exception
+    """
+    def __init__(self, line: str, message: str = "There was an error attempting to parse comments on line: {LINE}, \n check all the comments in your config are valid") -> None:
+        super().__init__(message.format(LINE=line))
